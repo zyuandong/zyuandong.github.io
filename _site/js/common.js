8adbf0defda1e3ebd5eee07a956c5083cd95c245
20111719 <de1719@163.com>
2015-05-02 22:43:38 +0800

    modify file

--- _site/js/common.js
+++ _site/js/common.js
@@ -1,3 +1,17 @@
 $(function(){
-	$("body").css('height', $(window).get(0).innerHeight);
+	$('body').css('height', $(window).get(0).innerHeight);
+	
+	$('#slider').on('click', function(){
+		if($(this).hasClass('left')){
+			$(this).removeClass('left').addClass('right');
+			$('#slide-panel').animate({
+				marginLeft: "-520px"
+			},500);
+		}else{
+			$(this).removeClass('right').addClass('left');
+			$('#slide-panel').animate({
+				marginLeft: "0"
+			},500);
+		}
+	});
 });
