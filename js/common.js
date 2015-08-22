5057d886ae52e43df8f64c0d54eb13eae206d5ec
bakerz <de1719@163.com>
2015-08-22 23:43:47 +0800

    see #31

--- js/common.js
+++ js/common.js
@@ -7,19 +7,26 @@ $(function(){
 	});
 	
 	$('#slider').on('click', function(){
-		if($(this).children('i').hasClass('toRight')){
-			$(this).children('i').remove();
-			$(this).html('<i class="fa fa-angle-left toLeft"></i>');
+		if($('#icon-slider').hasClass('fullscreen')) {
+			$('#icon-slider').removeClass('fullscreen');
 			$('#slide-panel').animate({
 				marginLeft: '0'
 			},500);
-		}else{
-			//$(this).children('i').removeClass('toLeft').addClass('toRight');
-			$(this).children('i').remove();
-			$(this).html('<i class="fa fa-angle-right toRight"></i>');
+		} else {
+			$('#icon-slider').addClass('fullscreen');
 			$('#slide-panel').animate({
 				marginLeft: '-640px'
 			},500);
 		}
+		/**
+		if($(this).children('i').hasClass('toRight')){
+			$(this).children('i').remove();
+			$(this).html('<i class="fa fa-angle-left toLeft"></i>');
+		}else{
+			//$(this).children('i').removeClass('toLeft').addClass('toRight');
+			$(this).children('i').remove();
+			$(this).html('<i class="fa fa-angle-right toRight"></i>');
+		}
+		**/
 	});
 });
