0958428da48ad89eae99f2aec4cf591adfc4de85
bakerz <de1719@163.com>
2015-09-02 17:16:29 +0800

    fix #18

--- _site/js/common.js
+++ _site/js/common.js
@@ -1,6 +1,4 @@
 $(function(){
-	//onScroll();
-
 	$('body').css('height', $(window).get(0).innerHeight);
 
 	$(window).resize(function() {
@@ -18,23 +16,14 @@ $(function(){
 			$('#icon-slider').addClass('fullscreen');
 			$('#slide-panel').animate({
 				marginLeft: '-640px'
-			},500).dequeue();
+			}, 500).dequeue();
 		}
-		/**
-		if($(this).children('i').hasClass('toRight')){
-			$(this).children('i').remove();
-			$(this).html('<i class="fa fa-angle-left toLeft"></i>');
-		}else{
-			//$(this).children('i').removeClass('toLeft').addClass('toRight');
-			$(this).children('i').remove();
-			$(this).html('<i class="fa fa-angle-right toRight"></i>');
-		}
-		**/
 	});
+
 	if($(window).width() <= 640) {
 		$('#slide-panel').animate({
 			marginLeft: '-640px'
-		},500).dequeue();
+		}, 500).dequeue();
 		$('#icon-slider').addClass('fullscreen');
 	}
 
@@ -52,4 +41,9 @@ $(function(){
 	    }
 	});
 
+	$('#back2top').click(function() {
+		$('#contents').animate({
+			scrollTop: '0'
+		}, 700);
+	});
 });
