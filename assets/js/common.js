6520da624cda61b056e2308f788d6ad2634cb1dc
bakerz <de1719@163.com>
2016-06-16 08:57:18 +0800

    common.js:modified js file

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,38 @@
 $(function(){
+    // switch file category tag
+    $('#slide-panel .guid-item').on('click', function() {
+        var page = $(this).data('page');
+        $(this).addClass('active').siblings().removeClass('active');
+        if (page) {
+            $('#slide-panel .page-item').hide();
+            $('#slide-panel').find('.' + page).show().not('.articles-all').css('display','flex');
+        }
+    });
+
+    // get categories-list
+    $('.category-all .category-item').on('click', function() {
+        var key = $(this).data('category');
+        $('.category-box ul').each(function() {
+            if ($(this).data('cateBox') == key) {
+                $(this).show().siblings().hide();
+            }
+        });
+
+        bindActive();
+    });
+
+    // get tags-list
+    $('.tags-all .tags-item').on('click', function() {
+        var key = $(this).data('tag');
+        $('.tags-box ul').each(function() {
+            if ($(this).data('tagBox') == key) {
+                $(this).show().siblings().hide();
+            }
+        });
+
+        bindActive();
+    });
+
     // click toggle-btn to show or hide slide-panel
     $('#toggle').on('click', function() {
         if ($(this).hasClass('fullscreen')) {
@@ -25,7 +59,7 @@ $(function(){
             });
         }
     });
-    
+
     // show back2top-btn
     $('#contents').scroll(function() {
         var t = $(this).scrollTop();
@@ -37,14 +71,14 @@ $(function(){
             $('#back2top').hide();
         }
     });
-    
+
     // back to top
     $('#back2top').click(function() {
         $('#contents').animate({
             scrollTop: '0'
         }, 700);
     });
-    
+
     // show or hide sidebar list
     $('#sidebar-btn').click(function() {
         if ($(this).hasClass('show')){
@@ -59,12 +93,12 @@ $(function(){
             }, 300).dequeue();
         }
     });
-    
+
     // mask-layer btn
     $('.mask-layer-btn').click(function() {
         $('#mask-layer').hide().find('.mask-layer-img').html('');
-    })
-    
+    });
+
     // use jquery-pjax
     bindActive();
     $(document).pjax('[data-pjax] a, a[data-pjax]', '#contents', {fragment: '#contents', timeout: 10000});
@@ -83,45 +117,17 @@ $(function(){
             pjaxEnd();
         }
     });
-    
-    // get categories-list
-    $('.category-all .category-item').on('click', function() {
-        var key = $(this).data('category');
-        $('.category-box ul').each(function() {
-            if ($(this).data('cateBox') == key) {
-                $(this).show().siblings().hide();
-            }
-        });
 
-        bindActive();
-    });
-    
-    // get tags-list
-    $('.tags-all .tags-item').on('click', function() {
-        var key = $(this).data('tag');
-        $('.tags-box ul').each(function() {
-            if ($(this).data('tagBox') == key) {
-                $(this).show().siblings().hide();
-            }
-        });
-
-        bindActive();
+    // focus list active
+    $('.sidebar-panel-ul li a').click(function() {
+        var index = $(this).data('index');
+        $('.pin').css('top', index * 24 + 5 + 'px');
     });
     
     pjaxEnd();
 });
     
 function pjaxEnd(){
-    // switch file category tag
-    $('#slide-panel .guid-item').on('click', function() {
-        var page = $(this).data('page');
-        $(this).addClass('active').siblings().removeClass('active');
-        if (page) {
-            $('#slide-panel .page-item').hide();
-            $('#slide-panel').find('.' + page).show().not('.articles-all').css('display','flex');
-        }
-    });
-
     // menu automatic hide in mobile
     if ($(window).width() <= 640) {
         $('#slide-panel').animate({
@@ -150,7 +156,7 @@ function pjaxEnd(){
     var html = '',
         topArr = [];
     $('#sidebar-panel .pin').css('top', '5px');
-    $('.date-tags').nextAll('h1,h2,h3,h4,h5,h6').each(function(i) {
+    $('.blog-content').find('h1,h2,h3,h4,h5,h6').each(function(i) {
         switch (this.tagName) {
             case 'H1':
                 topArr.push($(this).get(0).offsetTop);
@@ -203,12 +209,6 @@ function pjaxEnd(){
             }
         }
     })
-
-    // focus list active
-    $('.sidebar-panel-ul li a').click(function() {
-        var index = $(this).data('index');
-        $('.pin').css('top', index * 24 + 5 + 'px');
-    });
 
     // show image with full screen
     $('#contents img').click(function() {
