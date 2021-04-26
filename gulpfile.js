19a495aa521ebbe1e0d2d83696d43eda4a692e74
zyuandong <de1719@163.com>
2021-04-27 02:40:30 +0800

    feat(less): gulp create watch task to less -> css

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-const { watch, src, dest } =  require('gulp');
+const { watch, src, dest, series, parallel } =  require('gulp');
 const less = require('gulp-less');
+const clean = require('gulp-clean');
 const path = require('path');
 
 
-const watchTask = function() {
-  watch(['./assets/**/*.less'], function() {
-    return src('./assets/less/style.less')
-      .pipe(less({
-        paths: [ path.join(__dirname, 'less', 'includes') ]
-      }))
-      .pipe(dest('./assets/css'));
-  });  
+const cleanTask = () => {
+  return src('./assets/css/style.css', {read: false})
+    .pipe(clean());
 }
 
-exports.default = watchTask
+const lessTask = () => {
+  return src('./assets/less/style.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(dest('./assets/css'));
+}
+
+const watchTask = () => {
+  return watch(['./assets/**/*.less'], series(cleanTask, lessTask));  
+}
+
+exports.watch = watchTask;
+exports.default = watchTask;
